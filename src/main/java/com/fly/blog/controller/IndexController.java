8f3bf4e19a0a6af19207740c70c97024a229a76e
lifanyu <1509984575@qq.com>
2019-11-21 20:59:09 +0800

    添加页面原型并进行了页面的抽取

--- src/main/java/com/fly/blog/controller/IndexController.java
+++ src/main/java/com/fly/blog/controller/IndexController.java
@@ -1,14 +1,14 @@
 package com.fly.blog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "admin/blogs";
     }
 
 }
